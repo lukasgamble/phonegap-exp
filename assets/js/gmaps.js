72bfde16d4b30cd521d59d76af4c8bd3dd42c907
lukasgamble <lukas@lukasgamble.com>
2013-03-28 12:33:18 +0000

    update spring

--- assets/js/gmaps.js
+++ assets/js/gmaps.js
@@ -1,6 +1,3 @@
-/* *********************************************************
-GEO-LOCATION
-********************************************************* */
 var getLocation = function(){
         //alert("finding location")
         navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);   
@@ -28,6 +25,12 @@ function onGeoSuccess(position) {
                 alert('Geocoder failed due to: ' + status);
             }
         });
+        
+        // return the position object
+        return position;
+    
+    
+    
         //var element = document.getElementById('geolocation');
         //element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
         //		    'Longitude: '          + position.coords.longitude             + '<br />' +
@@ -44,70 +47,66 @@ function onGeoError(error) {
         alert('code: '    + error.code    + '\n' +
             'message: ' + error.message + '\n');
 }
-/* *********************************************************
-END GEO-LOCATION
-		********************************************************* */
 
+// VehoGroup Ltd. HQ
 var vehoMap = { 'center': '51.530052,-0.101023', 'zoom': 10};
-//51.7206744,-4.761887900000033
 
 $('#gps_map').live('pageinit', function() {
-        //demo.add('gps_map', function() {
-                $('#loadsMap').gmap({
-                    'center': vehoMap.center,
-                    'zoom': vehoMap.zoom,
-                    'disableDefaultUI':false,
-                    'mapTypeControl' : false, 
-                    'navigationControl' : true,
-                    'streetViewControl' : false, 
-                    'callback': function(map) {
-                        var self = this;
-                        self.watchPosition(function(position, status) {
-                                
-                                
-                                if ( status === 'OK' ) {
-                                        var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                                        
-                                        
-                                        // convert to address
-                                        geocoder = new google.maps.Geocoder();
-                                        geocoder.geocode({'latLng': latlng}, function(results, status) {
-                                            if (status == google.maps.GeocoderStatus.OK) {
-                                                if (results[1]) {
-                                                        //alert(results[1].formatted_address);
-                                                        $("#userDataStore").data({"geoAddress": results[1].formatted_address});
-                                                        $("#aroundMeInfoTitle").html("Current Location");
-                                                        $("#aroundMeInfoContent").html(results[1].formatted_address);
-                                                } else {
-                                                        alert('No results found');
-                                                        }
-                                            } else {
-                                                alert('Geocoder failed due to: ' + status);
-                                            }
-                                        });
-                                        
-                                        
-                                        // are there any markers currently loaded?
-                                        if ( !self.get('markers').client ) {
-                                                // no marker, so add one
-                                                self.addMarker({
-                                                        'id': 'client',
-                                                        'position': latlng,
-                                                        //'bounds': true,
-                                                        // need to pass in data in a function
-                                                        title: $("#userDataStore").data("firstName") + ' ' + $("#userDataStore").data("lastName")
-                                                });
-                                                map.setCenter(latlng);
-                                                map.setZoom(vehoMap.zoom);
-                                                
-                                        } else {
-                                                self.get('markers').client.setPosition(latlng);
-                                                //map.panTo(latlng);
-                                        }
+
+    $('#loadsMap').gmap({
+        'center': vehoMap.center,
+        'zoom': vehoMap.zoom,
+        'disableDefaultUI':false,
+        'mapTypeControl' : false, 
+        'navigationControl' : true,
+        'streetViewControl' : false, 
+        'callback': function(map) {
+            
+            
+            var self = this;
+            
+            
+            var pos = getLocation();
+            if (pos){
+                var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+                // convert to address
+                geocoder = new google.maps.Geocoder();
+                geocoder.geocode({'latLng': latlng}, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        if (results[1]) {
+                                //alert(results[1].formatted_address);
+                                $("#userDataStore").data({"geoAddress": results[1].formatted_address});
+                                $("#aroundMeInfoTitle").html("Current Location");
+                                $("#aroundMeInfoContent").html(results[1].formatted_address);
+                        } else {
+                                alert('No results found');
                                 }
+                    } else {
+                        alert('Geocoder failed due to: ' + status);
+                    }
+                });
+                
+                
+                // are there any markers currently loaded?
+                if ( !self.get('markers').client ) {
+                        // no marker, so add one
+                        self.addMarker({
+                                'id': 'client',
+                                'position': latlng,
+                                //'bounds': true,
+                                // need to pass in data in a function
+                                title: $("#userDataStore").data("firstName") + ' ' + $("#userDataStore").data("lastName")
                         });
-                }});
-        //}).load('gps_map');
+                        map.setCenter(latlng);
+                        map.setZoom(vehoMap.zoom);
+                        
+                } else {
+                        self.get('markers').client.setPosition(latlng);
+                        //map.panTo(latlng);
+                }
+            }
+           
+    }});
 });
 
 $('#gps_map').live('pageshow', function() {
